45e8774d7d564c1c26111791c06f23d5a8ff2b42
Kumar Raj <rajbhkseries@gmail.com>
2026-02-14 22:04:00 +0530

    written the Update()

--- models.go
+++ models.go
@@ -19,6 +19,18 @@ func initialModel() articles {
 		article: []article{{
 			url:      "www.google.com",
 			headline: "I'm feeling lucky",
+		}, {
+			url:      "www.youtube.com",
+			headline: "Let's watch some videos",
+		}, {
+			url:      "mail.google.com",
+			headline: "Check your emails regularly!!",
+		}, {
+			url:      "www.wikipedia.com",
+			headline: "Let's learn something",
+		}, {
+			url:      "www.learnspanish.com",
+			headline: "learn some spanish.",
 		}},
 		cursor: 0,
 	}
@@ -27,4 +39,29 @@ func initialModel() articles {
 func (m articles) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
 	return nil
+}
+
+func (m articles) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	// is it a key press???
+	case tea.KeyMsg:
+		// cool, what was the actual key pressed?
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.article)-1 {
+				m.cursor++
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m articles) View() string {
+	return "hoho"
 }
