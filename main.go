7088f224bbd9548727de42d30ca971706843de34
Kumar Raj <rajbhkseries@gmail.com>
2026-02-15 23:56:43 +0530

    Added pflags. This is so clean. when all the flags are not passed, a very beautiful message is given

--- main.go
+++ main.go
@@ -6,20 +6,26 @@ package main
 // dependencies.
 import (
 	"fmt"
+	flag "github.com/spf13/pflag"
 	"log"
-	// "os"
+	"os"
 	// tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	month := flag.StringP("month", "m", "", "Month Name")
+	year := flag.IntP("year", "y", 2026, "Year 2022 to 2026")
+	if *month == "" {
+		fmt.Println("Error: --month or -m is required.")
+		flag.Usage() // Prints the help menu automatically
+		os.Exit(1)   // Exit with an error code
+	}
 	// p := tea.NewProgram(initialModel())
 	// if _, err := p.Run(); err != nil {
 	// 	fmt.Printf("Ohhoo there has been an Error: %v", err)
 	// 	os.Exit(1)
 	// }
-	month := "january"
-	year := 2025
-	articles, err := scrape(month, year)
+	articles, err := scrape(*month, *year)
 	if err != nil {
 		log.Fatal(err)
 	}
